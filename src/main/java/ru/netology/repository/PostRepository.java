33878001b70d14186278c5b6316634ed4009f26a
Muratova Svetlana <muratova.sa@phystech.edu>
2022-12-04 14:39:24 +0300

    Annotation

--- src/main/java/ru/netology/repository/PostRepository.java
+++ src/main/java/ru/netology/repository/PostRepository.java
@@ -1,5 +1,6 @@
 package ru.netology.repository;
 
+import org.springframework.stereotype.Repository;
 import ru.netology.exception.NotFoundException;
 import ru.netology.model.Post;
 
@@ -12,6 +13,7 @@ import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.atomic.AtomicLong;
 
 // Stub
+@Repository
 public class PostRepository {
     private final ConcurrentMap<Long, Post> map = new ConcurrentHashMap<>();
     private final AtomicLong counter = new AtomicLong(0);
